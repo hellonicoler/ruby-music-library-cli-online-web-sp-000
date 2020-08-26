7275a3055cc77e98b38e1d34bc7f2f589bf091eb
Nicole Rothenay <nthorn1211@gmail.com>
2020-08-26 16:02:20 +0000

    Automatically backed up by Learn

--- lib/Genre.rb
+++ lib/Genre.rb
@@ -1,34 +1,38 @@
 class Genre
+  extend Concerns::Findable
+
   attr_accessor :name
   attr_reader :songs
-  
+
   @@all = []
-  
+
   def initialize(name)
     @name = name
     @songs = []
   end
-  
+
   def self.all
     @@all
   end
-  
+
   def self.destroy_all
-    @@all.clear
+    all.clear
   end
-  
+
   def save
-    @@all << self
+    self.class.all << self
   end
-  
-  def self.create(genre)
-    genre = self.new(genre)
+
+  def self.create(name)
+    genre = new(name)
     genre.save
     genre
+
+    # Or, as a one-liner:
+    # new(name).tap{ |g| g.save }
   end
-  
-    def artists
+
+  def artists
     songs.collect{ |s| s.artist }.uniq
   end
-  
 end
